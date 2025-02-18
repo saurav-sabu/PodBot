6e93178669070cad76f64fdba60585a64f91a6e4
Saurav Sabu <saurav.sabu9@gmail.com>
2025-02-18 19:32:54 +0530

    updated code in app.py

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ from gtts import gTTS
 import tempfile 
 import urllib.request  # Ensure urllib.request is available
 import re
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 
 st.set_page_config(
